48ccbdda52530d4be7f030e3643ed7a30c3e6372
Khloud355 <khloudrammah69@gmail.com>
2025-05-24 22:51:37 +0300

    enhnacements in design

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -4,28 +4,22 @@ import { SharedService } from 'src/app/auth/services/shared.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  userName:string =''
-  dropdownOpen:boolean=false
-  dateNow
-  constructor(private sharedService:SharedService){
-    this.dateNow = new Date()
-
+  userName: string = '';
+  dropdownOpen: boolean = false;
+  dateNow;
+  constructor(private sharedService: SharedService) {
+    this.dateNow = new Date();
   }
   ngOnInit() {
-    this.sharedService.getUserName().subscribe(result=>{
-      this.userName = result
-
-    })
-    
+    this.sharedService.getUserName().subscribe((result) => {
+      this.userName = result;
+    });
   }
- 
-  toggleDropdown(){
-    this.dropdownOpen= ! this.dropdownOpen
-    
-  }
-  
 
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
 }
